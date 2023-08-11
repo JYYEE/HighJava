96bfbf663cfbc618eee991f974a245bcede3dc85
PC-03 <PC-03@DESKTOP-VROBB5F>
2023-08-11 18:11:16 +0900

    loggingTest

---
+++ loggingTest/src/kr/or/ddit/basic/LoggingTest.java
@@ -0,0 +1,22 @@
+package kr.or.ddit.basic;
+
+import org.apache.log4j.Logger;
+
+public class LoggingTest {
+	// 1. Logger 클래스의 인스턴스를 받아온다.  
+	static Logger logger = Logger.getLogger(LoggingTest.class);
+	//클래스에서 발생하는 로그를 받아서 캡쳐하겠다는 의미.
+	
+	
+	public static void main(String[] args) {
+		// 2. 로그 기록을 남기는 방법
+		// 형식) Logger객체변수. 로그레벨명("출력할 메시지")
+		logger.trace("이것은 Log4J의 Trace레벨의 메시지입니다.");
+		logger.debug("이것은 Log4J의 debug레벨의 메시지입니다.");
+		logger.info("이것은 Log4J의 info레벨의 메시지입니다.");
+		logger.warn("이것은 Log4J의 warn레벨의 메시지입니다.");
+		logger.error("이것은 Log4J의 error레벨의 메시지입니다.");
+		logger.fatal("이것은 Log4J의 fatal레벨의 메시지입니다.");
+		
+	}
+}
---
+++ loggingTest/src/kr/or/ddit/mvc/service/MemberServiceImpl.java
@@ -0,0 +1,57 @@
+package kr.or.ddit.mvc.service;
+
+import java.util.List;
+import java.util.Map;
+
+import kr.or.ddit.mvc.dao.IMemberDao;
+import kr.or.ddit.mvc.dao.MemberDaoImpl;
+import kr.or.ddit.mvc.vo.MemberVO;
+
+public class MemberServiceImpl implements IMemberService {
+	// 일을 시킬 DAO객체 변수 선언 -- 필드 선언은 인터페이스로 하고 객체 생성은 클래스로 사용 
+	private IMemberDao dao;
+	
+	
+	// 생성자
+	public MemberServiceImpl() {
+		dao = new MemberDaoImpl(); // DAO 객체 생성
+	}
+	
+
+	@Override
+	public int insertMember(MemberVO memVO) {
+		return dao.insertMember(memVO);
+	}
+
+
+	@Override
+	public int deleteMember(String memId) {
+		return dao.deleteMember(memId);
+	}
+
+	@Override
+	public int updateMember(MemberVO memVO) {
+		return dao.updateMember(memVO);
+	}
+
+	@Override
+	public List<MemberVO> getAllMember() {
+		return dao.getAllMember();
+	}
+
+	@Override
+	public int getMemberCount(String memId) {
+		return dao.getMemberCount(memId);
+	}
+
+	@Override
+	public int updateMember2(Map<String, String> paramMap) {
+		return dao.updateMember2(paramMap);
+	}
+	
+	@Override
+	public int updateMember3(Map<String, String> dataMap) {
+		return dao.updateMember3(dataMap);
+	}
+
+}
